accd494d5e54e6a1b64b7349399bb4ff8d10e5a0
nlu <natlu@users.noreply.github.com>
2022-01-04 23:57:19 +1100

    add soln up to 8

--- Racket/hw4.rkt
+++ Racket/hw4.rkt
@@ -21,4 +21,44 @@
         [(= len-xs 0) (error "list-nth-mod: empty list")]
         [#t (car (list-tail xs (remainder n len-xs)))])))
 
-        
+; question 4
+(define (stream-for-n-steps s n)
+  (if (> n 0)
+      (cons (car (s)) (stream-for-n-steps (cdr (s)) (- n 1)))
+      null))
+
+; question 5
+(define funny-number-stream
+  (letrec ([funny-five (lambda (y) (if (= (remainder y 5) 0)
+                                        (* y -1)
+                                        y))]
+           [f (lambda (x) (cons (funny-five x) (lambda () (f (+ x 1)))))])
+  (lambda () (f 1))))
+
+ 
+; question 6
+(define dan-the-dog
+  (letrec ([dan (lambda () (cons "dan.jpg" dog))]
+           [dog (lambda () (cons "dog.jpg" dan))])
+    (lambda () (dan))))
+
+; question 7
+(define (stream-add-zero s)
+  (letrec ([f (lambda (s) (cons (cons 0 (car (s)))
+                                (lambda () (f (cdr (s))))))])
+    (lambda () (f s))))
+
+; question 8
+(define (cycle-lists xs ys)
+  (letrec ([f (lambda (n) (cons (cons (list-nth-mod xs n) (list-nth-mod ys n))
+                                (lambda () (f (+ n 1)))))])
+     (lambda () (f 0))))
+           
+
+; question 9
+
+
+
+
+
+
